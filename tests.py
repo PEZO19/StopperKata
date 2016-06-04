552a6d39ddcfdef6215059587baef59a50def23b
pezo <pezo1919@gmail.com>
2016-06-04 11:45:28 +0200

    [green] test__nostop__elapsed_0

--- tests.py
+++ tests.py
@@ -20,3 +20,9 @@ class StopperTest(unittest.TestCase):
         stopper = Stopper()
         elapsed_time = stopper.get_elapsed_time()
         self.assertEqual( elapsed_time, 0)
+
+    def test__nostop__elapsed_0(self):
+        stopper = Stopper()
+        stopper.start()
+        elapsed_time = stopper.get_elapsed_time()
+        self.assertEqual( elapsed_time, 0)
