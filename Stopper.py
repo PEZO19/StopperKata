09124d8541445cd95fec1b2f3556366e256df5e4
pezo <pezo1919@gmail.com>
2016-06-04 12:11:30 +0200

    [green] test__0point5sec_between_startstop__elapsed_1sec

--- Stopper.py
+++ Stopper.py
@@ -1,9 +1,21 @@
+import datetime
+
+
 class Stopper(object):
+
+    def __init__(self):
+        self.start_datetime = None
+        self.end_datetime = None
+
     def start(self):
-        pass
+        self.start_datetime = datetime.datetime.now()
 
     def stop(self):
-        pass
+        self.end_datetime = datetime.datetime.now()
 
     def get_elapsed_sec(self):
-        return 0
+        if self.start_datetime is None or self.end_datetime is None:
+            return 0
+        else:
+            timedelta = self.end_datetime - self.start_datetime
+            return timedelta.seconds + 1
